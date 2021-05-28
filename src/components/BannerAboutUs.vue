95b22b6b2efba7558702093825cf56dd581a41ff
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:50:45 -0500

    Create BannerAboutUs.vue

---
+++ src/components/BannerAboutUs.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="about-section section-spacing">
+    <div class="about-section-info">
+      <h2 class="about-section-info-title">Sobre nosotros</h2>
+      <p class="about-section-info-text">
+        Este es el sitio web de nuestra agencia de desarrollo.
+      </p>
+      <a href="/proyectos.html" class="btn btn-primary btn-ghost"
+        >Ver experiencia</a
+      >
+    </div>
+
+    <div class="about-section-background">
+      <img src="~/assets/images/jpg/bg-section.jpg" alt="Alumnos estudiando" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BannerAbout",
+};
+</script>
