bf690f15ad0080fc76c7906e4a364908d36c528a
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:52:04 -0500

    Create projects.vue

---
+++ src/pages/projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <Projects />
+</template>
+
+<script>
+import Projects from "~/components/Projects";
+
+export default {
+  name: "ProjectsPage",
+  components: {
+    Projects,
+  },
+};
+</script>
