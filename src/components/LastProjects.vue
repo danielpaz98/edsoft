4b904f02f353e8efd24cfa4fbdfb8141c519e1b1
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:50:57 -0500

    Create LastProjects.vue

---
+++ src/components/LastProjects.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="last-projects section-spacing">
+    <h2 class="last-projects-title">Últimos proyectos</h2>
+
+    <div class="container">
+      <a href="#" class="project-item">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h4 class="hero-card-info-title">Titulo proyecto</h4>
+            <p class="hero-card-info-text">Descripción proyecto</p>
+          </div>
+
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-1.jpg"
+            alt="Proyecto 1"
+          />
+        </div>
+      </a>
+
+      <a href="#" class="project-item">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h4 class="hero-card-info-title">Titulo proyecto</h4>
+            <p class="hero-card-info-text">Descripción proyecto</p>
+          </div>
+
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-2.jpg"
+            alt="Proyecto 2"
+          />
+        </div>
+      </a>
+
+      <a href="#" class="project-item">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h4 class="hero-card-info-title">Titulo proyecto</h4>
+            <p class="hero-card-info-text">Descripción proyecto</p>
+          </div>
+
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-3.jpg"
+            alt="Proyecto 3"
+          />
+        </div>
+      </a>
+
+      <a href="#" class="project-item">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h4 class="hero-card-info-title">Titulo proyecto</h4>
+            <p class="hero-card-info-text">Descripción proyecto</p>
+          </div>
+
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-4.jpg"
+            alt="Proyecto 4"
+          />
+        </div>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LastProjects",
+};
+</script>
