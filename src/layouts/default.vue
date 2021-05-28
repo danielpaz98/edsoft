55af5ef905d95aec3aba626e944370abcd14078c
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:40 -0500

    Create default.vue

---
+++ src/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-fluid">
+    <MainHeader />
+    <main>
+      <Nuxt />
+    </main>
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+import MainHeader from "~/components/MainHeader";
+import MainFooter from "~/components/MainFooter";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    MainHeader,
+    MainFooter,
+  },
+};
+</script>
