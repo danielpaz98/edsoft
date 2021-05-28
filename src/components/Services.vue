9b19edd92cf13f8e78fd6cc0e1a9821080cdb054
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:19 -0500

    Create Services.vue

---
+++ src/components/Services.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="services-section section-spacing">
+    <h2 class="services-section-title">Nuestros servicios</h2>
+
+    <div class="container">
+      <article class="service-item">
+        <span class="jam jam-phone"></span>
+        <h3>Servicio 1</h3>
+        <p>Diseñamos tu presencia en internet con apariencia profesional</p>
+      </article>
+
+      <article class="service-item">
+        <span class="jam jam-computer"></span>
+        <h3>Servicio 2</h3>
+        <p>Diseñamos tu presencia en internet con apariencia profesional</p>
+      </article>
+
+      <article class="service-item">
+        <span class="jam jam-keyboard"></span>
+        <h3>Servicio 3</h3>
+        <p>Diseñamos tu presencia en internet con apariencia profesional</p>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Services",
+};
+</script>
