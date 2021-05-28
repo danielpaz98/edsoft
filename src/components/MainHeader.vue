2a2dccf2d3edb3236da3c0ecff92c4235c4ecf8f
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:06 -0500

    Create MainHeader.vue

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="main-header">
+    <div class="container">
+      <nuxt-link to="/">
+        <img
+          class="main-header-logo"
+          src="~/assets/images/svg/logo.svg"
+          alt="Logo EDsoft"
+        />
+      </nuxt-link>
+
+      <input
+        type="checkbox"
+        id="btn-toggle-theme"
+        class="toggle-switch toggle-color-mode"
+        :checked="checked"
+        @click="changeTheme()"
+      />
+
+      <MainMenu />
+    </div>
+  </header>
+</template>
+
+<script>
+import MainMenu from "~/components/MainMenu";
+
+export default {
+  name: "MainHeader",
+  components: {
+    MainMenu,
+  },
+  data: () => {
+    return {
+      prueba: false,
+    };
+  },
+  methods: {
+    changeTheme() {
+      this.$colorMode.preference = this.toggleMode;
+    },
+  },
+  computed: {
+    toggleMode() {
+      return this.$colorMode.preference === "light" ? "dark" : "light";
+    },
+    checked() {
+      return this.$colorMode.preference === "light" ? false : true;
+    },
+  },
+};
+</script>
