3f1aa73c26e4fa3c13cb2058ee4eebdf9a2d9cd7
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:15 -0500

    Create Projects.vue

---
+++ src/components/Projects.vue
@@ -0,0 +1,173 @@
+<template>
+  <section class="projects-section section-spacing">
+    <h1 class="projects-section-title">Nuestros Proyectos</h1>
+
+    <div class="container">
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 1</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-1.jpg"
+            alt="Proyecto 1"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 2</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-2.jpg"
+            alt="Proyecto 2"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 3</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-3.jpg"
+            alt="Proyecto 3"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 4</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-4.jpg"
+            alt="Proyecto 4"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 5</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-5.jpg"
+            alt="Proyecto 5"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 1</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-1.jpg"
+            alt="Proyecto 1"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 2</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-2.jpg"
+            alt="Proyecto 2"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 3</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-3.jpg"
+            alt="Proyecto 3"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 4</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-4.jpg"
+            alt="Proyecto 4"
+          />
+        </div>
+      </a>
+
+      <a href="https://australia.gov.au" target="_blank">
+        <div class="hero-card">
+          <div class="hero-card-info">
+            <h3 class="hero-card-info-title">Proyecto 5</h3>
+            <p class="hero-card-info-text">
+              Construcción de aplicación web para nuestro cliente en Australia
+            </p>
+          </div>
+          <img
+            class="hero-card-img"
+            src="~/assets/images/jpg/project-5.jpg"
+            alt="Proyecto 5"
+          />
+        </div>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+};
+</script>
