59b30d39229a7eac71923398090277aeb6c23a41
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:01 -0500

    Create MainFooter.vue

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="main-footer">
+    <p>
+      Todos los derechos reservados &copy;
+      {{ new Date().getFullYear() }}
+      | EDsoft tech labs agency
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "MainFooter",
+};
+</script>
