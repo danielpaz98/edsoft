a0895151c3cc58e7cab7526feebbb3d91acf9781
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:50:52 -0500

    Create ContactUs.vue

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="contact-section section-spacing">
+    <h1 class="contact-section-title">Contacto</h1>
+
+    <div class="container">
+      <div class="contact-section-info">
+        <h3 class="contact-section-info-title">¿Tienes algo que decir?</h3>
+
+        <div class="contact-section-info-content">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fyouwinbo&tabs=messages&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=268863463136938"
+            width="340"
+            height="500"
+            style="border: none; overflow: hidden"
+            scrolling="no"
+            frameborder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        </div>
+      </div>
+
+      <address>
+        <div class="contact-item">
+          <h4>Teléfono</h4>
+          <p>
+            <span class="jam jam-phone"></span>
+            <a href="tel:1234567890">(123) - 456 - 7890</a>
+          </p>
+        </div>
+
+        <div class="contact-item">
+          <h4>Correo electrónico</h4>
+          <p>
+            <span class="jam jam-newsletter"></span>
+            <a href="mailto:contacto@edsoft.com">contacto@edsoft.com</a>
+          </p>
+        </div>
+
+        <div class="contact-item">
+          <h4>Redes sociales</h4>
+          <div class="social-networks">
+            <a href="#"><span class="jam jam-twitter"></span></a>
+            <a href="#"><span class="jam jam-facebook"></span></a>
+            <a href="#"><span class="jam jam-whatsapp"></span></a>
+            <a href="#"><span class="jam jam-instagram"></span></a>
+          </div>
+        </div>
+
+        <div class="contact-item">
+          <h4>Dirección</h4>
+          <p>
+            <span class="jam jam-map-marker"></span>
+            <a
+              href="https://www.google.com/maps/place/Groenlandia/@67.8825871,-73.8807304,3z/data=!3m1!4b1!4m5!3m4!1s0x4ea20dbbe3c07715:0x34cf9d830114e218!8m2!3d65.8747247!4d-46.1425781"
+              target="_blank"
+              >Calle Falsa #123, Springfield, Óregon, Estados Unidos</a
+            >
+          </p>
+        </div>
+
+        <iframe
+          class="map-ubcation"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24611878.567421794!2d-73.88073035826869!3d67.88258712523397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ea20dbbe3c07715%3A0x34cf9d830114e218!2sGroenlandia!5e0!3m2!1ses!2smx!4v1589145084108!5m2!1ses!2smx"
+          frameborder="0"
+          allowfullscreen=""
+          style="border: 0"
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </address>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
