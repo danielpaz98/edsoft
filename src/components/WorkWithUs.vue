ba091d4187d2953f8bfa11e0573497d6d62ae061
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:24 -0500

    Create WorkWithUs.vue

---
+++ src/components/WorkWithUs.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="work-with-us section-spacing">
+    <div class="container">
+      <div class="work-with-us-info">
+        <h2 class="work-with-us-info-title">¿Quieres trabajar con nosotros?</h2>
+
+        <p class="work-with-us-info-subtitle">
+          Es momento de convertir tu idea en algo real. Puedes contactarnos en
+          nuestras redes sociales<br />
+          o enviando un correo a:
+        </p>
+
+        <a href="mailto:contacto@edsoft.com" class="btn btn-primary"
+          >contacto@edsoft.com</a
+        >
+      </div>
+
+      <div class="social-networks">
+        <a href="#" target="_blank">
+          <span class="jam jam-facebook"></span>
+        </a>
+
+        <a href="#" target="_blank">
+          <span class="jam jam-twitter"></span>
+        </a>
+
+        <a href="#" target="_blank">
+          <span class="jam jam-whatsapp"></span>
+        </a>
+
+        <a href="#" target="_blank">
+          <span class="jam jam-instagram"></span>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "WorkWithUs",
+};
+</script>
