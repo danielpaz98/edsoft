0fa1b3c6b3e27d8de8f62d2656d9a791a1327f24
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:58 -0500

    Create index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <BannerInfo />
+    <Services />
+    <BannerAboutUs />
+    <LastProjects />
+    <WorkWithUs />
+  </div>
+</template>
+
+<script>
+import BannerInfo from "~/components/BannerInfo";
+import Services from "~/components/Services";
+import BannerAboutUs from "~/components/BannerAboutUs";
+import LastProjects from "~/components/LastProjects";
+import WorkWithUs from "~/components/WorkWithUs";
+
+export default {
+  name: "IndexPage",
+  components: {
+    BannerInfo,
+    Services,
+    BannerAboutUs,
+    LastProjects,
+    WorkWithUs,
+  },
+};
+</script>
