4a75f443494aceecf1f326728943b4f79b0374ba
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:50:41 -0500

    Create AboutUs.vue

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="about-us section-spacing">
+    <h1 class="about-us-title">¿Quienes somos?</h1>
+
+    <div class="container">
+      <article class="about-item">
+        <div class="image-container">
+          <img src="~/assets/images/jpg/about-1.jpg" alt="Sobre nosotros" />
+        </div>
+
+        <div class="about-item-info">
+          <h2 class="about-item-info-title">Sobre nosotros</h2>
+          <p class="about-item-info-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            aliquam, neque id tempus consequat, mi nibh luctus nibh, sit amet
+            auctor enim ligula nec arcu. Nulla efficitur elementum euismod. In
+            hac habitasse platea dictumst. Pellentesque habitant morbi tristique
+            senectus et netus et malesuada fames ac turpis egestas.
+          </p>
+        </div>
+      </article>
+
+      <article class="about-item">
+        <div class="image-container">
+          <img src="~/assets/images/jpg/about-2.jpg" alt="Sobre nosotros" />
+        </div>
+
+        <div class="about-item-info">
+          <h2 class="about-item-info-title">Misión</h2>
+          <p class="about-item-info-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            aliquam, neque id tempus consequat, mi nibh luctus nibh, sit amet
+            auctor enim ligula nec arcu. Nulla efficitur elementum euismod. In
+            hac habitasse platea dictumst. Pellentesque habitant egestas.
+          </p>
+        </div>
+      </article>
+
+      <article class="about-item">
+        <div class="image-container">
+          <img src="~/assets/images/jpg/about-3.jpg" alt="Sobre nosotros" />
+        </div>
+
+        <div class="about-item-info">
+          <h2 class="about-item-info-title">Visión</h2>
+          <p class="about-item-info-text">
+            Neque id tempus consequat, mi nibh luctus nibh, sit amet auctor enim
+            ligula nec arcu. Nulla efficitur elementum euismod. In hac habitasse
+            platea dictumst. Pellentesque habitant morbi tristique senectus et
+            netus et malesuada fames ac turpis egestas.
+          </p>
+        </div>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutUs",
+};
+</script>
