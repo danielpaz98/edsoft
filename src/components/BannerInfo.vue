b82d3cb24b630400dfddf33b7b3f475b623a3085
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:50:49 -0500

    Create BannerInfo.vue

---
+++ src/components/BannerInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="main-banner section-spacing">
+    <div class="container">
+      <div class="main-banner-info">
+        <h1 class="main-banner-info-title">Bienvenidos a EDsoft</h1>
+        <p class="main-banner-info-subtitle">
+          Necesitas acesoramiento urgente!!! Nosotros te podemos ayudar en la
+          construcción de tu sitio web.
+        </p>
+
+        <a class="btn btn-white" href="/contacto.html">Escríbenos ya mismo!</a>
+      </div>
+
+      <div class="main-banner-img">
+        <img src="~/assets/images/svg/computer.svg" alt="Computadora EDsoft" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BannerInfo",
+};
+</script>
