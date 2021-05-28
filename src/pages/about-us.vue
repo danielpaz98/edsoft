7110b876c46b1f8137b387958ee378080e37cabd
Daniel Paz <alexdaniel-1998@outlook.com>
2021-05-28 16:51:50 -0500

    Create about-us.vue

---
+++ src/pages/about-us.vue
@@ -0,0 +1,14 @@
+<template>
+  <AboutUsUs />
+</template>
+
+<script>
+import AboutUsUs from "~/components/AboutUs";
+
+export default {
+  name: "AboutUsPage",
+  components: {
+    AboutUsUs,
+  },
+};
+</script>
